a95c3fdd323f0f4f52b4215882a4d45b51c338c6
Max Yen <maxyen123@gmail.com>
2025-06-28 13:33:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ if not df.empty:
     display_df = filtered_df[existing_display_columns]
     display_df = display_df.rename(columns=display_column_map)
 
-    st.markdown("<p style='text-align: right; color: grey;'><i>AI Sentiment Analysis, Average Score*</i></p>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: right; color: grey; padding-right: 5%;'><i>AI Sentiment Analysis, Average Score*</i></p>", unsafe_allow_html=True)
 
     st.dataframe(
         display_df,
